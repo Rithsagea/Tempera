4b70ee39375bbf7579003cea88d7ae97430656bf
rithsagea <rithsagea@rithsageas-imac.home>
2019-08-30 17:39:44 -0400

    Intial Commit

---
+++ src/com/rithsagea/tempera/api/StatContainer.java
@@ -0,0 +1,13 @@
+package com.rithsagea.tempera.api;
+
+public class StatContainer {
+	public int attack;
+	public int defense;
+	public int health;
+	
+	public StatContainer(int attack, int defense, int health) {
+		this.attack = attack;
+		this.defense = defense;
+		this.health = health;
+	}
+}
