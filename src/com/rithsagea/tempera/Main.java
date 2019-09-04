07171bb5cdd336332efc1541336dfa979dbb8193
Rithsagea <rithsagea@gmail.com>
2019-09-04 19:06:11 -0400

    Add some basic items

--- src/com/rithsagea/tempera/Main.java
+++ src/com/rithsagea/tempera/Main.java
@@ -1,7 +1,36 @@
 package com.rithsagea.tempera;
 
 public class Main {
+	//LEGENDARY
+	final Item TYRFING = new Item("Tyrfing", new StatContainer(20, 10, 0));
+	final Item EXCALIBUR = new Item("Excalibur", new StatContainer(30, 0, 0));
+	final Item KUSANAGI = new Item("Kusanagi", new StatContainer(20, 5, 5));
+	
+	final Item AEGIS = new Item("Aegis", new StatContainer(0, 30, 0));
+	
+	final Item PHILOSTONE = new Item("Philosopher's Stone", new StatContainer(0, 20, 20));
+	
+	//EPIC
+	final Item DURENDAL = new Item("Durendal", new StatContainer(15, 5, 0));
+	final Item MORALTA = new Item("Moralta", new StatContainer(20, 0, 0));
+	
+	final Item EITR = new Item("Eitr", new StatContainer(0, 15, 5));
+	final Item ADAMANT = new Item("Adamant", new StatContainer(0, 20, 0));
+	
+	//RARE
+	final Item DAMSTEELSWORD = new Item("Damascus Steel Sword", new StatContainer(5, 5, 0));
+	final Item RUNEDBLADE = new Item("Runed Blade", new StatContainer(5, 0, 5));
+	
+	final Item PINECONEBOW = new Item("Pinecone Bow", new StatContainer(5, 5, 0));
+	final Item VINEBOW = new Item("Vine Bow", new StatContainer(5, 0, 5));
+	final Item GLASSBOW = new Item("Glass Bow", new StatContainer(10, 0, 0));
+	
+	//COMMON
+	final Item MAGMA = new Item("Magma", new StatContainer(5, 0, 0));
+	final Item CRYSTAL = new Item("Crystal", new StatContainer(0, 5, 0));
+	final Item HONEY = new Item("Honey", new StatContainer(0, 0, 5));
+	
 	public static void main(String args[]) {
-		System.out.println("Hello World!");
+		
 	}
 }
--- src/com/rithsagea/tempera/StatContainer.java
+++ src/com/rithsagea/tempera/StatContainer.java
@@ -5,6 +5,12 @@ public class StatContainer {
 	public int defense;
 	public int health;
 	
+	/**
+	 * 
+	 * @param attack		value
+	 * @param defense	value
+	 * @param health		value
+	 */
 	public StatContainer(int attack, int defense, int health) {
 		this.attack = attack;
 		this.defense = defense;
