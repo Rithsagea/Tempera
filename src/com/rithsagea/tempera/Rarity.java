4d0e9bf52637ec505ebc7bb66f09bdf5577c4361
Rithsagea <rithsagea@gmail.com>
2019-09-04 21:18:03 -0400

    Added Prices

--- src/com/rithsagea/tempera/Rarity.java
+++ src/com/rithsagea/tempera/Rarity.java
@@ -1,15 +1,21 @@
 package com.rithsagea.tempera;
 
 public enum Rarity {
-	LEGENDARY(1),
-	EPIC(2),
-	RARE(8),
-	COMMON(64);
+	LEGENDARY(1, 1000),
+	EPIC(2, 250),
+	RARE(8, 50),
+	COMMON(64, 1);
 	
 	private int weight = 0;
+	private int cost = 0;
 	
-	Rarity(int weight) {
+	Rarity(int weight, int cost) {
 		this.weight = weight;
+		this.cost = cost;
+	}
+	
+	public int getCost() {
+		return cost;
 	}
 	
 	public int getWeight() {
