b707271556d6571b919164f0bcbf50f14001b0e1
Rithsagea <rithsagea@gmail.com>
2019-09-05 11:28:32 -0400

    Added some stats for item equipping

--- src/com/rithsagea/tempera/Player.java
+++ src/com/rithsagea/tempera/Player.java
@@ -7,5 +7,16 @@ public class Player {
 	
 	public Player(StatContainer stats) {
 		this.stats = stats;
-	}	
+	}
+	
+	public void attack(Monster monster) {
+		stats.health -= Math.max(0, monster.stats.attack - stats.defense);
+		monster.stats.health -= Math.max(0,  stats.attack - monster.stats.defense);
+	}
+	
+	public void equipItem(Item item) {
+		stats.subtract(item.statBonus);
+		heldItem = item;
+		stats.add(item.statBonus);
+	}
 }
--- src/com/rithsagea/tempera/StatContainer.java
+++ src/com/rithsagea/tempera/StatContainer.java
@@ -3,6 +3,7 @@ package com.rithsagea.tempera;
 public class StatContainer {
 	public int attack;
 	public int defense;
+	public int mhealth;
 	public int health;
 	
 	/**
@@ -14,6 +15,19 @@ public class StatContainer {
 	public StatContainer(int attack, int defense, int health) {
 		this.attack = attack;
 		this.defense = defense;
+		this.mhealth = health;
 		this.health = health;
+	}
+	
+	public void add(StatContainer container) {
+		this.attack += container.attack;
+		this.defense += container.defense;
+		this.health += container.health;
+	}
+	
+	public void subtract(StatContainer container) {
+		this.attack -= container.attack;
+		this.defense -= container.defense;
+		this.health -= container.health;
 	}
 }
