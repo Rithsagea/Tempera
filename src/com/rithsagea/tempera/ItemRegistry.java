222e61557dcf65d259d9d820e1a4cea849fdc18b
Rithsagea <rithsagea@gmail.com>
2019-09-04 21:18:22 -0400

    Created a registry and weighted loot table

---
+++ src/com/rithsagea/tempera/ItemRegistry.java
@@ -0,0 +1,52 @@
+package com.rithsagea.tempera;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ItemRegistry {
+	private static List<Item> registry = new ArrayList<Item>();
+	
+	private static Item[] items;
+	
+	private static int totalWeight;
+	private static int length;
+	
+	private static Random rand;
+	
+	public static void registerItem(Item item) {
+		registry.add(item);
+	}
+	
+	public static void registerItems(Item... item) {
+		for(Item obj : item) {
+			registry.add(obj);
+		}
+	}
+	
+	public static void init() {
+		rand = new Random(System.currentTimeMillis());
+		length = registry.size();
+		items = new Item[length];
+		items = registry.toArray(items);
+		for(Item item : items) {
+			totalWeight += item.rarity.getWeight();
+		}
+	}
+	
+	public static Item getItem() {
+		int total = rand.nextInt(totalWeight);
+		for(int x = 0; x < length; x++) {
+			total -= items[x].rarity.getWeight();
+			if(total <= 0)
+				return items[x];
+		}
+		return null;
+	}
+	
+	public static void printItems() {
+		for(Item item : items) {
+			System.out.println(item.name);
+		}
+	}
+}
--- src/com/rithsagea/tempera/Main.java
+++ src/com/rithsagea/tempera/Main.java
@@ -2,39 +2,85 @@ package com.rithsagea.tempera;
 
 public class Main {
 	//LEGENDARY
-	final Item TYRFING = new Item(Rarity.LEGENDARY, "Tyrfing", new StatContainer(20, 10, 0));
-	final Item EXCALIBUR = new Item(Rarity.LEGENDARY, "Excalibur", new StatContainer(30, 0, 0));
-	final Item KUSANAGI = new Item(Rarity.LEGENDARY, "Kusanagi", new StatContainer(20, 5, 5));
+	static final Item TYRFING = new Item(Rarity.LEGENDARY, "Tyrfing", new StatContainer(20, 10, 0));
+	static final Item EXCALIBUR = new Item(Rarity.LEGENDARY, "Excalibur", new StatContainer(30, 0, 0));
+	static final Item KUSANAGI = new Item(Rarity.LEGENDARY, "Kusanagi", new StatContainer(20, 5, 5));
 	
-	final Item AEGIS = new Item(Rarity.LEGENDARY, "Aegis", new StatContainer(0, 30, 0));
+	static final Item AEGIS = new Item(Rarity.LEGENDARY, "Aegis", new StatContainer(0, 30, 0));
 	
-	final Item PHILOSTONE = new Item(Rarity.LEGENDARY, "Philosopher's Stone", new StatContainer(0, 20, 20));
+	static final Item PHILOSTONE = new Item(Rarity.LEGENDARY, "Philosopher's Stone", new StatContainer(0, 20, 20));
 	
 	//EPIC
-	final Item DURENDAL = new Item(Rarity.EPIC, "Durendal", new StatContainer(15, 5, 0));
-	final Item MORALTA = new Item(Rarity.EPIC, "Moralta", new StatContainer(20, 0, 0));
-	final Item GOUJIAN = new Item(Rarity.EPIC, "Goujian", new StatContainer(15, 0, 5));
+	static final Item DURENDAL = new Item(Rarity.EPIC, "Durendal", new StatContainer(15, 5, 0));
+	static final Item MORALTA = new Item(Rarity.EPIC, "Moralta", new StatContainer(20, 0, 0));
+	static final Item GOUJIAN = new Item(Rarity.EPIC, "Goujian", new StatContainer(15, 0, 5));
 	
-	final Item EITR = new Item(Rarity.EPIC, "Eitr", new StatContainer(0, 15, 5));
-	final Item ADAMANT = new Item(Rarity.EPIC, "Adamant", new StatContainer(0, 20, 0));
+	static final Item EITR = new Item(Rarity.EPIC, "Eitr", new StatContainer(0, 15, 5));
+	static final Item ADAMANT = new Item(Rarity.EPIC, "Adamant", new StatContainer(0, 20, 0));
 	
 	//RARE
-	final Item DAMSTEELSWORD = new Item(Rarity.RARE, "Damascus Steel Sword", new StatContainer(5, 5, 0));
-	final Item RUNEDBLADE = new Item(Rarity.RARE, "Runed Blade", new StatContainer(5, 0, 5));
+	static final Item DAMSTEELSWORD = new Item(Rarity.RARE, "Damascus Steel Sword", new StatContainer(5, 5, 0));
+	static final Item RUNEDBLADE = new Item(Rarity.RARE, "Runed Blade", new StatContainer(5, 0, 5));
 	
-	final Item COBALTSHIELD = new Item(Rarity.RARE, "Cobalt Shield", new StatContainer(0, 5, 5));
-	final Item PERMAFROSTSHIELD = new Item(Rarity.RARE, "Permafrost Shield", new StatContainer(0, 10, 0));
+	static final Item COBALTSHIELD = new Item(Rarity.RARE, "Cobalt Shield", new StatContainer(0, 5, 5));
+	static final Item PERMAFROSTSHIELD = new Item(Rarity.RARE, "Permafrost Shield", new StatContainer(0, 10, 0));
 	
-	final Item PINECONEBOW = new Item(Rarity.RARE, "Pinecone Bow", new StatContainer(5, 5, 0));
-	final Item VINEBOW = new Item(Rarity.RARE, "Vine Bow", new StatContainer(5, 0, 5));
-	final Item GLASSBOW = new Item(Rarity.RARE, "Glass Bow", new StatContainer(10, 0, 0));
+	static final Item PINECONEBOW = new Item(Rarity.RARE, "Pinecone Bow", new StatContainer(5, 5, 0));
+	static final Item VINEBOW = new Item(Rarity.RARE, "Vine Bow", new StatContainer(5, 0, 5));
+	static final Item GLASSBOW = new Item(Rarity.RARE, "Glass Bow", new StatContainer(10, 0, 0));
 	
 	//COMMON
-	final Item MAGMA = new Item(Rarity.COMMON, "Magma", new StatContainer(5, 0, 0));
-	final Item CRYSTAL = new Item(Rarity.COMMON, "Crystal", new StatContainer(0, 5, 0));
-	final Item HONEY = new Item(Rarity.COMMON, "Honey", new StatContainer(0, 0, 5));
+	static final Item MAGMA = new Item(Rarity.COMMON, "Magma", new StatContainer(5, 0, 0));
+	static final Item CRYSTAL = new Item(Rarity.COMMON, "Crystal", new StatContainer(0, 5, 0));
+	static final Item HONEY = new Item(Rarity.COMMON, "Honey", new StatContainer(0, 0, 5));
+	
+	public static void registerItems() {
+		//LEGENDARY
+		ItemRegistry.registerItems(
+				TYRFING,
+				EXCALIBUR,
+				KUSANAGI,
+				AEGIS,
+				PHILOSTONE);
+		//EPIC
+		ItemRegistry.registerItems(
+				DURENDAL,
+				MORALTA,
+				GOUJIAN,
+				EITR,
+				ADAMANT);
+		//RARE
+		ItemRegistry.registerItems(
+				DAMSTEELSWORD,
+				RUNEDBLADE,
+				COBALTSHIELD,
+				PERMAFROSTSHIELD,
+				PINECONEBOW,
+				VINEBOW,
+				GLASSBOW);
+		//COMMON
+		ItemRegistry.registerItems(
+				MAGMA,
+				CRYSTAL,
+				HONEY);
+		
+		ItemRegistry.init();
+	}
 	
 	public static void main(String args[]) {
+		registerItems();
+		ItemRegistry.printItems();
 		
+		System.out.println("\n\nBegin Roll\n------------------------------------------");
+		Item item;
+		int cost = 0;
+		
+		for(int x = 0; x < 100; x++) {
+			item = ItemRegistry.getItem();
+			cost += item.rarity.getCost();
+			System.out.println(ItemRegistry.getItem().name);
+		}
+		
+		System.out.format("\nTotal Coins: %d", cost);
 	}
 }
