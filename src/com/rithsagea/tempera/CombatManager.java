38308e6454d5d27af7404c702ef373faa4d6cf5f
Rithsagea <rithsagea@gmail.com>
2019-09-12 11:20:18 -0400

    Added turns

--- src/com/rithsagea/tempera/CombatManager.java
+++ src/com/rithsagea/tempera/CombatManager.java
@@ -12,4 +12,18 @@ public class CombatManager {
 		this.player = player;
 		this.monster = monster;
 	}
+	
+	public boolean playerTurn() {
+		monster.stats.health -= Math.max(0, player.stats.attack - monster.stats.defense);
+		if(monster.stats.health <= 0)
+			return true; //the monster died
+		return false; //no one dies
+	}
+	
+	public boolean monsterTurn() {
+		player.stats.health -= Math.max(0, monster.stats.attack - player.stats.defense);
+		if(player.stats.health <= 0)
+			return true;
+		return false;
+	}
 }
