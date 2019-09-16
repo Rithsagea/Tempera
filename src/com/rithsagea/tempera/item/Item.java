59d05ae42783025a28bc928205450504cc271046
Rithsagea <rithsagea@gmail.com>
2019-09-15 20:52:45 -0400

    Added a string conversion and compare for sorting

--- src/com/rithsagea/tempera/item/Item.java
+++ src/com/rithsagea/tempera/item/Item.java
@@ -3,7 +3,7 @@ package com.rithsagea.tempera.item;
 import com.rithsagea.tempera.Rarity;
 import com.rithsagea.tempera.stat.StatContainer;
 
-public class Item {
+public class Item implements Comparable<Item>{
 	public StatContainer statBonus;
 	public String name;
 	public Rarity rarity;
@@ -14,5 +14,19 @@ public class Item {
 		this.name = name;
 		this.type = type;
 		statBonus = stats;
+	}
+	
+	public String toString() {
+		return name;
+	}
+
+	@Override
+	public int compareTo(Item o) {
+		if(o.rarity.ordinal() > rarity.ordinal()) //other object has higher rarity
+			return 1; //current object is less than other object
+		if(o.rarity.ordinal() < rarity.ordinal())
+			return -1;
+		
+		return name.compareTo(o.name);
 	}
 }
--- src/com/rithsagea/tempera/stat/Monster.java
+++ src/com/rithsagea/tempera/stat/Monster.java
@@ -2,7 +2,7 @@ package com.rithsagea.tempera.stat;
 
 import com.rithsagea.tempera.Rarity;
 
-public class Monster {
+public class Monster implements Comparable<Monster> {
 	public StatContainer stats;
 	public String name;
 	public Rarity rarity;
@@ -11,5 +11,19 @@ public class Monster {
 		this.name = name;
 		this.rarity = rarity;
 		this.stats = stats;
+	}
+	
+	public String toString() {
+		return name;
+	}
+	
+	@Override
+	public int compareTo(Monster o) {
+		if(o.rarity.ordinal() > rarity.ordinal()) //other object has higher rarity
+			return 1; //current object is less than other object
+		if(o.rarity.ordinal() < rarity.ordinal())
+			return -1;
+		
+		return name.compareTo(o.name);
 	}
 }
