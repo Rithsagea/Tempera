d9f5eab0128becd1c737a4262fb3d0452f0353e9
Rithsagea <rithsagea@gmail.com>
2019-09-17 11:36:16 -0400

    Changed the roll test. (it's better now i swear)

--- src/com/rithsagea/tempera/Main.java
+++ src/com/rithsagea/tempera/Main.java
@@ -1,7 +1,9 @@
 package com.rithsagea.tempera;
 
+import com.rithsagea.tempera.item.Item;
 import com.rithsagea.tempera.registry.ItemRegistry;
 import com.rithsagea.tempera.registry.MonsterRegistry;
+import com.rithsagea.tempera.stat.Monster;
 
 public class Main {
 	
@@ -18,15 +20,18 @@ public class Main {
 		System.out.println("\n-=-=- Monsters -=-=-");
 		MonsterRegistry.printMonsters();
 		
-		//Random tests I can delete later
-		DataManager.rollLootTest(Rarity.COMMON);
-		DataManager.rollLootTest(Rarity.UNCOMMON);
-		DataManager.rollLootTest(Rarity.RARE);
-		DataManager.rollLootTest(Rarity.EPIC);
-		DataManager.rollLootTest(Rarity.LEGENDARY);
+		int rarities[] = {0, 0, 0, 0, 0};
 		
-		DataManager.rollSpawnTest(Area.CERES);
-		DataManager.rollSpawnTest(Area.NEBULA);
-		DataManager.rollSpawnTest(Area.ANDROMEDA);
+		for(int x = 0; x < 100; x++) {
+			Monster monster = DataManager.getMonster(Area.ANDROMEDA);
+			Item item = DataManager.getLoot(monster.rarity);
+			rarities[item.rarity.ordinal()]++;
+			System.out.format("[%s] %s		[%s] %s\n", monster.rarity, monster, item.rarity, item);
+			
+		}
+		System.out.println();
+		for(int x = 0; x < 5; x++) {
+			System.out.format("You have found %d %s\n", rarities[x], Rarity.values()[x]);
+		}
 	}
 }
