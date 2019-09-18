a0d45ade20601b9f1cd982e22c5d5a56c8b673c1
Rithsagea <rithsagea@gmail.com>
2019-09-17 21:40:27 -0400

    Turn based combat

--- src/com/rithsagea/tempera/stat/CombatManager.java
+++ src/com/rithsagea/tempera/stat/CombatManager.java
@@ -1,5 +1,10 @@
 package com.rithsagea.tempera.stat;
 
+import java.util.Scanner;
+
+import com.rithsagea.tempera.DataManager;
+import com.rithsagea.tempera.item.Item;
+
 public class CombatManager {
 	
 	public Player player;
@@ -8,19 +13,44 @@ public class CombatManager {
 	public CombatManager(Player player, Monster monster) {
 		this.player = player;
 		this.monster = monster;
+		
+		printStatus();
+	}
+	
+	public Item run() {
+		while(playerTurn() == false) {
+			if(monsterTurn() == true) {
+				System.out.println("\n\nYou died!");
+				return null;
+			}
+		}
+		Item item = DataManager.getLoot(monster.rarity);
+		System.out.format("\n\n%s obtained %s", player, item);
+		return item;
 	}
 	
 	public boolean playerTurn() {
-		monster.stats.health -= Math.max(0, player.stats.attack - monster.stats.defense);
+		int damage = Math.max(1, player.stats.attack - monster.stats.defense);
+		monster.stats.health -= damage;
+		System.out.format("\n%s attacks the %s, dealing %d damage\n\n", player, monster, damage);
+		printStatus();
 		if(monster.stats.health <= 0)
 			return true; //the monster died
 		return false; //no one dies
 	}
 	
 	public boolean monsterTurn() {
-		player.stats.health -= Math.max(0, monster.stats.attack - player.stats.defense);
+		int damage = Math.max(1, monster.stats.attack - player.stats.defense);
+		player.stats.health -= damage;
+		System.out.format("\nThe %s attacks %s, dealing %d damage\n\n", monster, player, damage);
+		printStatus();
 		if(player.stats.health <= 0)
 			return true;
 		return false;
+	}
+	
+	public void printStatus() {
+		System.out.format("-=-=- %s -=-=-\n%s", monster, monster.stats);
+		System.out.format("\n\n-=-=- %s -=-=-\n%s\n", player, player.stats);
 	}
 }
