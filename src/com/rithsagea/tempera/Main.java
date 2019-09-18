b4904f661477caabf1916bc6a2efc5a7b8ad4b9b
Rithsagea <rithsagea@gmail.com>
2019-09-18 11:35:54 -0400

    Commented out debug tests

--- src/com/rithsagea/tempera/Main.java
+++ src/com/rithsagea/tempera/Main.java
@@ -1,12 +1,6 @@
 package com.rithsagea.tempera;
 
-import com.rithsagea.tempera.item.Item;
-import com.rithsagea.tempera.registry.ItemRegistry;
-import com.rithsagea.tempera.registry.MonsterRegistry;
-import com.rithsagea.tempera.stat.CombatManager;
-import com.rithsagea.tempera.stat.Monster;
-import com.rithsagea.tempera.stat.Player;
-import com.rithsagea.tempera.stat.StatContainer;
+import com.rithsagea.tempera.ui.Game;
 
 public class Main {
 	
@@ -17,32 +11,36 @@ public class Main {
 		DataManager.registerLoot();
 		DataManager.registerSpawns();
 		
-		System.out.println("-=-=- Items -=-=-");
-		ItemRegistry.printItems();
+		//Debug Message
+//		System.out.println("-=-=- Items -=-=-");
+//		ItemRegistry.printItems();
+//		
+//		System.out.println("\n-=-=- Monsters -=-=-");
+//		MonsterRegistry.printMonsters();
+
+ 		//For testing weights
+//		int rarities[] = {0, 0, 0, 0, 0};
+//
+//		for(int x = 0; x < 100; x++) {
+//			Monster monster = DataManager.getMonster(Area.ANDROMEDA);
+//			Item item = DataManager.getLoot(monster.rarity);
+//			rarities[item.rarity.ordinal()]++;
+//			System.out.format("[%s] %s		[%s] %s\n", monster.rarity, monster, item.rarity, item);
+//			
+//		}
+//		System.out.println();
+//		for(int x = 0; x < 5; x++) {
+//			System.out.format("You have found %d %s\n", rarities[x], Rarity.values()[x]);
+//		}
 		
-		System.out.println("\n-=-=- Monsters -=-=-");
-		MonsterRegistry.printMonsters();
+		//Testing Combat
+//		Monster monster = DataManager.getMonster(Area.NEBULA);
+//		Player player = new Player("Rithsagea", new StatContainer(10, 10, 10));
+//		
+//		CombatManager manager = new CombatManager(player, monster);
+//		manager.run();
 		
-		int rarities[] = {0, 0, 0, 0, 0};
+		Game window = new Game();
 		
-		for(int x = 0; x < 100; x++) {
-			Monster monster = DataManager.getMonster(Area.ANDROMEDA);
-			Item item = DataManager.getLoot(monster.rarity);
-			rarities[item.rarity.ordinal()]++;
-			System.out.format("[%s] %s		[%s] %s\n", monster.rarity, monster, item.rarity, item);
-			
-		}
-		System.out.println();
-		for(int x = 0; x < 5; x++) {
-			System.out.format("You have found %d %s\n", rarities[x], Rarity.values()[x]);
-		}
-		
-		System.out.print("\n\n\n\n");
-		
-		Monster monster = DataManager.getMonster(Area.NEBULA);
-		Player player = new Player("Rithsagea", new StatContainer(10, 10, 10));
-		
-		CombatManager manager = new CombatManager(player, monster);
-		manager.run();
 	}
 }
