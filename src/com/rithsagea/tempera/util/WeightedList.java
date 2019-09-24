2bf191d8d03572ea3a409aecfd5c92b563bc8e82
Rithsagea <rithsagea@gmail.com>
2019-09-24 11:20:45 -0400

    Removed some redundant type thing.

--- src/com/rithsagea/tempera/util/WeightedList.java
+++ src/com/rithsagea/tempera/util/WeightedList.java
@@ -4,20 +4,20 @@ import java.util.Random;
 
 public class WeightedList<T> {
 	
-	private class Node<K> {
-		 public K element;
+	private class Node {
+		 public T element;
 		 public int weight;
-		 public Node<K> child;
+		 public Node child;
 		 
-		 public Node(K element, int weight) {
+		 public Node(T element, int weight) {
 			 this.element = element;
 			 this.weight = weight;
 			 this.child = null;
 		 }
 	}
 	
-	private final Node<T> root = new Node<T>(null, 0);
-	private Node<T> top = root;
+	private final Node root = new Node(null, 0);
+	private Node top = root;
 	private Random rand = new Random();
 	
 	protected int totalWeight = 0;
@@ -26,14 +26,14 @@ public class WeightedList<T> {
 	public void put(T element, int weight) {
 		totalWeight += weight;
 		elements++;
-		Node<T> node = new Node<T>(element, weight);
+		Node node = new Node(element, weight);
 		top.child = node;
 		top = node;
 	}
 	
 	public T getElement() {
 		int value = rand.nextInt(totalWeight) + 1;
-		Node<T> temp = root;
+		Node temp = root;
 		
 		while(temp.child != null) {
 			temp = temp.child;
