53509fc8674b0d52cc4423fa9b9ced6069f16868
utinj <utinj123@gmail.com>
2019-11-25 22:32:13 -0500

    made class for playing audio

--- src/com/tempera/audio/Audio.java
+++ src/com/tempera/audio/Audio.java
@@ -7,12 +7,14 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 public class Audio {
+
 	public void playSound(String file) {
 		try {
 			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(file).getAbsoluteFile());
 			Clip clip = AudioSystem.getClip();
 			clip.open(audioInputStream);
 			clip.start();
+			
 		} catch(Exception ex) {
 			System.out.println("Error with playing sound.");
 			ex.printStackTrace();
