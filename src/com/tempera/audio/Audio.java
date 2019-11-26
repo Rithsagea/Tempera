86531476734b0147218112461f19117c5f30a7e3
utinj <utinj123@gmail.com>
2019-11-25 22:23:09 -0500

    new class to play audio

---
+++ src/com/tempera/audio/Audio.java
@@ -0,0 +1,22 @@
+package com.tempera.audio;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Audio {
+	public void playSound(String file) {
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(file).getAbsoluteFile());
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+		} catch(Exception ex) {
+			System.out.println("Error with playing sound.");
+			ex.printStackTrace();
+		}
+	}
+
+}
