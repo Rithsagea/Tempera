05680f10d292505e2975e4870e274c3b0cadc55c
Rithsagea <rithsagea@gmail.com>
2020-01-23 11:41:27 -0500

    Collisions resolution Works!

--- src/tempera/listeners/CollisionListener.java
+++ src/tempera/listeners/CollisionListener.java
@@ -16,30 +16,43 @@ public class CollisionListener implements Listener {
 		double[] edgesA = A.boundingBox.getEdges();
 		double[] edgesB = B.boundingBox.getEdges();
 		
-		Point centerA = A.boundingBox.getCenter();
-		Point centerB = B.boundingBox.getCenter();
+		//halve velocity, because it sounds like a good idea
 		
-		//Calculate movement change for object A
+		A.velocity.divide(2);
+		B.velocity.divide(2);
 		
-		double horizontalVel = Math.min(centerA.getX() - edgesB[0], edgesB[1] - centerA.getX());
-		double verticalVel = Math.min(centerA.getY() - edgesB[2], edgesB[3] - centerA.getY());
+		//Make objects not be stuck together
 		
-		System.out.println(horizontalVel);
-		System.out.println(verticalVel);
+		double weightSum = A.weight + B.weight;
 		
-		//Calculate movement change for object B
+		double horizontalDiff = (Math.min(edgesA[1] - edgesB[0], edgesB[1] - edgesA[0])) / weightSum;
+		double verticalDiff = (Math.min(edgesA[3] - edgesB[2], edgesB[3] - edgesA[2])) / weightSum;
 		
-		double force = A.weight * A.velocity.getMagnitude()
-				+ B.weight * B.velocity.getMagnitude() / 2;
-		
-		
-		Vector direction = new Vector(A.position, B.position);
-		
-		if(A.weight != 0)
-			A.velocity.add(direction.inverse().setMagnitude(force / A.weight));
-		if(B.weight != 0)
-			B.velocity.add(direction.setMagnitude(force / B.weight));
-		
-//		System.out.println(force);
+		if(horizontalDiff < verticalDiff) {	//the collision is horizontal
+			//velocity
+			double horizontalForce = A.weight * A.velocity.getX()
+									+B.weight * B.velocity.getX();
+			horizontalForce *= -1;
+			
+			if(A.position.getX() > B.position.getX())
+				horizontalDiff *= -1;
+			
+			A.position.add(-horizontalDiff * A.weight, 0);
+			B.position.add(horizontalDiff * B.weight, 0);
+			A.velocity.add(-horizontalForce * A.weight, 0);
+			B.velocity.add(horizontalForce * B.weight, 0);
+		} else { //the collision is vertical
+			double verticalForce = A.weight * A.velocity.getY()
+					+B.weight * B.velocity.getY();
+			verticalForce *= -1;
+			
+			if(A.position.getY() > B.position.getY())
+				verticalDiff *= -1;
+			
+			A.position.add(0, -verticalDiff * A.weight);
+			B.position.add(0, verticalDiff * B.weight);
+			A.velocity.add(0, -verticalForce * A.weight);
+			B.velocity.add(0, verticalForce * B.weight);
+		}
 	}
 }
