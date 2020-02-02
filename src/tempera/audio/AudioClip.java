82d72acaec96e895bd32e12f4f6ce8515b16b9df
Rithsagea <rithsagea@gmail.com>
2020-02-02 18:54:10 -0500

    Custom audio works. (kind of janky)

--- src/tempera/audio/AudioClip.java
+++ src/tempera/audio/AudioClip.java
@@ -7,22 +7,14 @@ import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-import sun.audio.AudioData;
-
 public class AudioClip {
-	
-	private AudioData audioData;
 	
 	private AudioFormat format;
 	private int frames;
 	private int frameSize;
 	private int bytes;
-	private float frameRate;
 	private byte[] data;
 	
 	public AudioClip(File audioFile) throws UnsupportedAudioFileException, IOException {
@@ -32,22 +24,21 @@ public class AudioClip {
 		format = ais.getFormat();
 		frames = aff.getFrameLength();
 		frameSize = format.getFrameSize();
-		frameRate = format.getFrameRate();
 		bytes = frames * frameSize;
 		
 		data = new byte[bytes];
 		ais.read(data);
 	}
 	
-	public void play() throws LineUnavailableException {
-		SourceDataLine line = null;
-		DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-		line = (SourceDataLine) AudioSystem.getLine(info);
-		line.open(format);
-		
-		line.start();
-		line.write(data,  0, bytes);
-		line.drain();
-		line.close();
+	public byte[] getData() {
+		return data;
+	}
+	
+	public AudioFormat getFormat() {
+		return format;
+	}
+	
+	public int getLengthInBytes() {
+		return bytes;
 	}
 }
--- test/test/tempera/audio/AudioTest.java
+++ test/test/tempera/audio/AudioTest.java
@@ -7,11 +7,20 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 import tempera.audio.AudioClip;
+import tempera.audio.AudioPlayer;
 
 public class AudioTest {
-	public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-		AudioClip clip = new AudioClip(new File("src/resources/songDemo.wav"));
-		clip.play();
-		System.out.println(Integer.MAX_VALUE);
+	public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
+		AudioClip clip1 = new AudioClip(new File("src/resources/songDemo.wav"));
+		AudioClip clip2 = new AudioClip(new File("src/resources/soundDemo.wav"));
+		AudioClip clip3 = new AudioClip(new File("src/resources/songdemo2.wav"));
+		
+		
+		AudioPlayer player = new AudioPlayer();
+		player.play(clip1);
+		
+		Thread.sleep(1000);
+		
+		player.stop();
 	}
 }
