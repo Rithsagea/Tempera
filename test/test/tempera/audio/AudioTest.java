180b01e029f3d8322bae032cec5b063cd6503619
Rithsagea <rithsagea@gmail.com>
2020-02-02 20:28:38 -0500

    Removed Clutter

--- test/test/tempera/audio/AudioTest.java
+++ test/test/tempera/audio/AudioTest.java
@@ -12,16 +12,8 @@ import tempera.audio.AudioPlayer;
 public class AudioTest {
 	public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
 		AudioClip clip1 = new AudioClip(new File("src/resources/songDemo.wav"));
-		AudioClip clip2 = new AudioClip(new File("src/resources/soundDemo.wav"));
-		AudioClip clip3 = new AudioClip(new File("src/resources/songdemo2.wav"));
-		
 		
 		AudioPlayer player1 = new AudioPlayer();
 		player1.play(clip1);
-		
-		AudioPlayer player2 = new AudioPlayer();
-		
-		Thread.sleep(3000);
-		player2.play(clip1);
 	}
 }
